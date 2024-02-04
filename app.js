8e7c21967801415ece0ee4f52f2e0b5d4695063f
Evgeniy Kozlov <ek916569@gmail.com>
2024-02-04 17:37:09 +0500

    add ssh tunnel and mongodb connect to server

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 import express from "express";
 import swaggerUI from "swagger-ui-express";
 import swaggerJSDocument from "swagger-jsdoc";
+import mongoose from "mongoose";
+import { createTunnel } from "tunnel-ssh";
 
 const app = express();
 const router = express.Router();
@@ -34,6 +36,36 @@ app.use(
 
 app.use("/api", router);
 
+createTunnel(
+	{},
+	{
+		port: 27017,
+	},
+	{
+		host: "83.147.246.28",
+		port: 22,
+		username: "root",
+		password: "cAPbb@L+@k94,G",
+	},
+	{
+		srcAddr: "127.0.0.1",
+		srcPort: 27017,
+		dstAddr: "127.0.0.1",
+		dstPort: 27017,
+	},
+).then(([_, __], error) => {
+	if (error) {
+		console.log("[SSH TUNNEL ERROR]");
+	} else {
+		mongoose
+			.connect(
+				"mongodb://127.0.0.1:27017/?serverSelectionTimeoutMS=2000&appName=mongosh+2.1.3&directConnection=true&tls=false",
+			)
+			.then(_ => console.log("[DATABASE OK]"))
+			.catch(_ => console.log("[DATABASE ERROR]"));
+	}
+});
+
 /**
  * @swagger
  * /settings:
@@ -52,8 +84,9 @@ app.use("/api", router);
  *     200:
  *        description: Успешно
  */
-router.route("/settings")
+router
+	.route("/settings")
 	.get((_, res) => res.send("Return settings"))
 	.patch((req, res) => res.send("Change setting or settings"));
 
-app.listen(9999, () => console.log("[SERVER STARTED]"));
+app.listen(9999, () => console.log("[SERVER OK]"));
